c35913826d2cce527719d3b428ecad3447db6a03
dlfrnaos19 <dlfrnaos19@gmail.com>
2022-08-05 20:40:58 +0900

    need one more

--- programmers/camoflage.py
+++ programmers/camoflage.py
@@ -14,3 +14,11 @@ def solution(clothes):
         result *= len(i)
     result = result - 1
     return result
+
+#다른 사람의 풀이
+# def solution(clothes):
+#     from collections import Counter
+#     from functools import reduce
+#     cnt = Counter([kind for name, kind in clothes])
+#     answer = reduce(lambda x, y: x*(y+1), cnt.values(), 1) - 1
+#     return answer
